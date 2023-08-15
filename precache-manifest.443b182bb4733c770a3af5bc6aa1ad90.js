6975fc3aa1f924d18500ecfb06f1f353f025ac53
Vincent Basile <vpbphone@gmail.com>
2023-08-14 23:44:30 -0400

    Updates

---
+++ precache-manifest.443b182bb4733c770a3af5bc6aa1ad90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "08f045561b8e8962313b",
+    "url": "/trivial-endeavor/static/css/main.95cf221b.chunk.css"
+  },
+  {
+    "revision": "08f045561b8e8962313b",
+    "url": "/trivial-endeavor/static/js/main.08f04556.chunk.js"
+  },
+  {
+    "revision": "a1a3b12e01986321e5df",
+    "url": "/trivial-endeavor/static/js/1.a1a3b12e.chunk.js"
+  },
+  {
+    "revision": "653de6590ef646d45d68",
+    "url": "/trivial-endeavor/static/js/runtime~main.653de659.js"
+  },
+  {
+    "revision": "1c763c0977e13f46e71a76b6c4930a49",
+    "url": "/trivial-endeavor/static/media/trivialEndeavorLogo0.1c763c09.svg"
+  },
+  {
+    "revision": "17693b0767f3887a1c6833d7077cd454",
+    "url": "/trivial-endeavor/static/media/Precious.17693b07.ttf"
+  },
+  {
+    "revision": "c3e97087be8e911458d20ee33c177741",
+    "url": "/trivial-endeavor/index.html"
+  }
+];
